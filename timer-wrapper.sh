cdd92f918f8106adc54f03deb79bdea35fae211a
Philipp Matthias Schaefer <philipp.matthias.schaefer@posteo.de>
2018-02-19 17:57:23 +0100

    (Try to) prevent skipping images

--- timer-wrapper.sh
+++ timer-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this calls the archillect-image-get-and-display script every 10 minutes.
 while true
-do 
-    [[ $((10#`date +%M`%10)) == 0 ]] && ./archillect.sh
-    sleep 60
+do
+    [[ $((10#`date +%M`%10)) == 1 ]] && ./archillect.sh
+    sleep 59
 done
